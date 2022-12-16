d9eab7fb6c31192cd93e867274ff3e8df0499213
ilya <ilia.arkhangelsky@gmail.com>
2022-12-16 16:48:05 +0300

    Рабочая версия программы, определяющей четное число или нет

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine () ?? "";
+
+int number = int.Parse(userInput);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ("Число является чётным");
+}
+else
+{
+Console.WriteLine ("Число не является чётным");
+}
+
+
+
